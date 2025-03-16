77b64670214afcb77e2311eb1e55cfd780a1930f
Stephan Kulla <git.mail@kulla.me>
2025-03-16 12:11:09 +0100

    feat: Add WikimediaProject.api_query()

---
+++ wikimedia.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class WikimediaProject:
+    def __init__(
+        self, domain: str = "de.wikibooks.org", http_client=requests.Session()
+    ):
+        self.domain = domain
+        self.http_client = http_client
+
+    def api_query(self, action: str, **params) -> dict:
+        params["action"] = action
+        params["format"] = "json"
+        response = self.http_client.get(
+            f"https://{self.domain}/w/api.php", params=params
+        )
+        response.raise_for_status()
+        return response.json()["query"]
