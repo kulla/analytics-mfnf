c0ca9963ecde56a0944a8e52e1a8c0fe55a3ba96
Stephan Kulla <git.mail@kulla.me>
2025-03-16 12:45:18 +0100

    test: Refactor names in wikimedia_test.py

--- tests/wikimedia_test.py
+++ tests/wikimedia_test.py
@@ -10,22 +10,22 @@ def mock_http_client():
 
 
 @pytest.fixture
-def wikimedia_project():
+def wikimedia_client():
     return WikimediaAPIClient()
 
 
 @pytest.fixture
-def mocked_wikimedia_project(mock_http_client):
+def mocked_wikimedia_client(mock_http_client):
     return WikimediaAPIClient(http_client=mock_http_client)
 
 
-def test_api_query(wikimedia_project):
-    result = wikimedia_project.api_query(prop="info", titles="Mathe für Nicht-Freaks")
+def test_api_query_success(wikimedia_client):
+    result = wikimedia_client.api_query(prop="info", titles="Mathe für Nicht-Freaks")
 
     assert result["pages"]["68442"]["title"] == "Mathe für Nicht-Freaks"
 
 
-def test_api_query_raises_exception(mocked_wikimedia_project, mock_http_client):
+def test_api_query_http_error(mocked_wikimedia_client, mock_http_client):
     mock_response = Mock()
     mock_response.status_code = 404
     mock_response.raise_for_status.side_effect = HTTPError(
@@ -34,4 +34,4 @@ def test_api_query_raises_exception(mocked_wikimedia_project, mock_http_client):
     mock_http_client.get.return_value = mock_response
 
     with pytest.raises(HTTPError, match="404 Client Error: Not Found for url"):
-        mocked_wikimedia_project.api_query(titles="Python")
+        mocked_wikimedia_client.api_query(prop="info", titles="Python")
