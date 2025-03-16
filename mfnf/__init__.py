97df221caa2ada9cb6b3bdd98386bf5d6507f0a0
Stephan Kulla <git.mail@kulla.me>
2025-03-16 19:30:15 +0100

    feat(mfnf): Add books variable

--- mfnf/__init__.py
+++ mfnf/__init__.py
@@ -1,3 +1,13 @@
 from .mfnf import MFNF
 
-__all__ = ["MFNF"]
+books = [
+    "Analysis 1",
+    "Grundlagen der Mathematik",
+    "Lineare Algebra 1",
+    "Maßtheorie",
+    "Real Analysis",
+    "Linear algebra",
+    "Measure theory",
+]
+
+__all__ = ["MFNF", "books"]
